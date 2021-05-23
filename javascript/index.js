588aef4d5d5ba86094571b60d8f9db11332be43a
Michael Carter <michael_carter_21@hotmail.com>
2021-05-23 18:27:33 +0300

    fixed alignment issues on mobile devices

--- javascript/index.js
+++ javascript/index.js
@@ -1,31 +1,26 @@
 console.log('file was loaded');
 
 const nav = document.querySelector('#navigation');
-const body = document.querySelector('body');
-const navTop = navigation.offsetTop;
-
 const menu = document.querySelector('.menu');
 const menuToggle = document.querySelector('.menuicon');
 const closedMenu = document.querySelector('.menu-closed');
 const navList = document.querySelector('.nav-list');
+const menuList = document.querySelector('.menu-list');
 let isMenuOpen = false;
-
-// function stickyNavigation() {
-
-//   if (window.scrollY >= navTop) {
-//     document.getElementById('navigation').classList.add('sticky');
-//   } else {
-//     document.getElementById('navigation').classList.remove('sticky');
-//   }
-// }
-
-// window.addEventListener('scroll', stickyNavigation);
 
 // TOGGLE MENU ACTIVE STATE
 menuToggle.addEventListener('click', (e) => {
   e.preventDefault();
   isMenuOpen = !isMenuOpen;
 
+  nav.classList.toggle('nav--open');
+  closedMenu.classList.toggle('menu-open');
+  menu.classList.toggle('show');
+});
+
+console.log(navList);
+
+menuList.addEventListener('click', (e) => {
   nav.classList.toggle('nav--open');
   closedMenu.classList.toggle('menu-open');
   menu.classList.toggle('show');
