d7afb158a2383a72fbfb1c5098b6663fde9380e7
Michael Carter <michael_carter_21@hotmail.com>
2021-05-26 02:14:35 +0300

    animations added for landing page

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,29 @@ const menuToggle = document.querySelector('.menuicon');
 const closedMenu = document.querySelector('.menu-closed');
 const navList = document.querySelector('.nav-list');
 const menuList = document.querySelector('.menu-list');
+
 let isMenuOpen = false;
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('lottie'), // Required
+  path: 'https://assets1.lottiefiles.com/packages/lf20_47dwpmff.json', // Required
+  renderer: 'svg', // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+  name: 'Hello World', // Name for future reference. Optional.
+  initialSegment: [0, 90],
+});
+
+var animationScrollDown = bodymovin.loadAnimation({
+  container: document.getElementById('scroll-down-animation'), // Required
+  path: '../assets/lottie/scroll-down.json', // Required
+  renderer: 'svg', // Required
+  loop: true, // Optional
+  autoplay: true, // Optional
+  name: 'Hello World', // Name for future reference. Optional.
+});
+
+animationScrollDown.setSpeed(0.8);
 
 // TOGGLE MENU ACTIVE STATE
 menuToggle.addEventListener('click', (e) => {
@@ -21,3 +43,5 @@ menuList.addEventListener('click', (e) => {
   closedMenu.classList.toggle('menu-open');
   menu.classList.toggle('show');
 });
+
+animation.setAttribute.seg;
